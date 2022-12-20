182b07f2728d47684195987c5996d2684460d249
sujin <ikosdu60@gmail.com>
2022-12-20 20:27:06 +0900

    Feat: Router 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import SignIn from './ui/pages/SignIn'
-import SignUp from './ui/pages/SignUp'
-import TodoListPage from './ui/pages/TodoList'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import SignIn from './ui/pages/SignIn';
+import SignUp from './ui/pages/SignUp';
 
 const Router = () => (
   <Routes>
     <Route path="/" element={<SignIn />} />
     <Route path="/signup" element={<SignUp />} />
-    <Route path="/todo" element={<TodoListPage />} />
   </Routes>
-)
+);
 
-export default Router
+export default Router;
