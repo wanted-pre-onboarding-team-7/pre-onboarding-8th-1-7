7d823a6df39e27e93b233c16c74b72365d6af61b
Quartz <skyu.dev@gmail.com>
2022-12-21 19:25:11 +0900

    Chore: 폴더 구조 및 기본 세팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
+import Router from './routes/Router';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Router />
+    </BrowserRouter>
+  );
 }
 
 export default App;
