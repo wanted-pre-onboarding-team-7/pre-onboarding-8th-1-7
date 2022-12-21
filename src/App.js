b0ebb14014cba8739c964a6fe1ecbf4cddb6249a
jiyoonZ <wldbsl416@gmail.com>
2022-12-21 11:54:22 +0900

    Feat: 전역관리 스타일-라우터

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { HashRouter } from 'react-router-dom';
+import Router from './routes/Router';
+import GlobalStyle from './styles/GlobalStyle';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyle />
+      <HashRouter>
+        <Router />
+      </HashRouter>
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
+import { themeColor } from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={themeColor}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
