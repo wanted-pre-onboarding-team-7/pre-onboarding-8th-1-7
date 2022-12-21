376f6b245536b9b8c943ff112bfcf90f82df1bc7
eternalclash <77526745+eternalclash@users.noreply.github.com>
2022-12-21 16:27:19 +0900

    Revert "Soo"

--- src/App.js
+++ src/App.js
@@ -1,27 +1,5 @@
-import {Route,Routes} from "react-router-dom"
-import styled from "styled-components"
-import LoginPage from "./pages/LoginPage";
-import TodoListPage from "./pages/TodoListPage";
 function App() {
-
-  return (
-    <Main>
-     
-    <Routes>
-        <Route exact path="/"  element={<LoginPage/>} />
-        <Route exact path="/todo" element={<TodoListPage/>} />
-    </Routes>     
-   
-   </Main>
-  );
+  return <div className="App"></div>;
 }
-
-const Main = styled.div`
-  display:flex;
-  min-height:100vh;
-  justify-content:center;
-  align-items:center;
-
-`
 
 export default App;
