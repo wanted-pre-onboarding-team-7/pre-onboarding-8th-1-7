75af46b3348ed87e1097fcb1feb77091408ac94c
sujin <soojin9877@naver.com>
2022-12-21 14:13:25 +0900

    Feat: 라우팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Todo from './pages/Todo';
+
 function App() {
-  return <div className="App"></div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/todo" element={<Todo />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
