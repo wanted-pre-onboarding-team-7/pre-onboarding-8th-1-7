e8d1a785a7a2ac7c40063bd2ededb895a24c9323
khw970421 <khw970421@kakao.com>
2022-12-21 12:29:24 +0900

    Chore: React-Router 기본세팅 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LoginAndRegister, Todos, NotFound } from './pages';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginAndRegister />} />
+        <Route path="/todos" element={<Todos />} />
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
