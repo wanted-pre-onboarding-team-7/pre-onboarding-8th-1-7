ee1ae989e0adde7589ac4c76414c74a6c4fd65dd
Quartz <skyu.dev@gmail.com>
2022-12-21 11:46:12 +0900

    Feat: home, todo 라우팅 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import TodoScreen from './routes/TodoScreen';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/todo" element={<TodoScreen />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
