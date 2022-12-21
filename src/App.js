fa9b38fbe481146c36c8d29f4c39996b7060fd97
sujin <soojin9877@naver.com>
2022-12-21 14:17:36 +0900

    Feat: 회원가입 및 로그인 기능

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Home from './pages/Home';
 import Todo from './pages/Todo';
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/todo" element={<Todo />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,95 @@
-import React from 'react';
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import axios from 'axios';
+import { BASE_URL } from '../constants/constants';
 
 function Home() {
-  return <div>home</div>;
+  const token = localStorage.getItem('token');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passCheck, setPassCheck] = useState(false);
+
+  const handleCheckValid = () => {
+    const emailRegex = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+
+    if (email.match(emailRegex) && password.length >= 8) {
+      setPassCheck(true);
+    } else {
+      setPassCheck(false);
+    }
+  };
+
+  const handleSignUp = async () => {
+    try {
+      const response = await axios.post(`${BASE_URL}auth/signup`, {
+        email: email,
+        password: password,
+      });
+      if (response.status === 201) alert('회원가입이 완료되었습니다.');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleSignIn = async () => {
+    try {
+      const response = await axios.post(`${BASE_URL}auth/signin`, {
+        email: email,
+        password: password,
+      });
+      if (response.status === 200) {
+        localStorage.setItem('token', response.data.access_token);
+        window.location.replace('/todo');
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <>
+      {token && <Navigate to="/todo" replace={true} />}
+      <main>
+        <h1>로그인 및 회원가입</h1>
+        <form>
+          <div>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              onKeyUp={handleCheckValid}
+              placeholder="이메일"
+            ></input>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              onKeyUp={handleCheckValid}
+              placeholder="비밀번호"
+            ></input>
+          </div>
+          <button
+            type="button"
+            onClick={handleSignUp}
+            disabled={passCheck ? false : true}
+          >
+            회원가입
+          </button>
+          <button
+            type="button"
+            onClick={handleSignIn}
+            disabled={passCheck ? false : true}
+          >
+            로그인
+          </button>
+        </form>
+      </main>
+    </>
+  );
 }
 
 export default Home;
