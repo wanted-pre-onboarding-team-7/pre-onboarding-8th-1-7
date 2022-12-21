f7746c0f825e374fb92dedea1026149ce3bdaf21
khw970421 <khw970421@kakao.com>
2022-12-21 14:10:00 +0900

    Feat: 로그인 여부에 따른 리다이렉트 처리

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getItem } from '../utils/localStorage';
+import { useNavigate } from 'react-router-dom';
 
 const Todos = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = getItem('loginToken');
+    if (!token) {
+      navigate('/');
+    }
+  }, []);
   return <div>Todos</div>;
 };
 
