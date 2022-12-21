c27a3670cd265718740fba717b6f271b41c9b58f
jiyoonZ <wldbsl416@gmail.com>
2022-12-21 13:28:30 +0900

    Feat: todo 라우팅

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Auth from '../pages/Auth';
+import Todo from '../pages/Todo';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Auth />} />
+      <Route path="/todo" element={<Todo />} />
     </Routes>
   );
 }
