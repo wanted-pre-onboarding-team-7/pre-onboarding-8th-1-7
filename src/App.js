a6ca55a1ffe279059f8cb3d9833a5076ac1fef9b
ckwlghks123 <ckwlghks12+github2@gmail.com>
2022-12-21 14:12:13 +0900

    Feat: 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import TodoList from './pages/TodoList/TodoList';
+import Home from './pages/Home/Home';
+import SignUp from './pages/SignUp/SignUp';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="sign-up" element={<SignUp />} />
+        </Route>
+        <Route path="/todo" element={<TodoList />} />
+        <Route path="/*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
