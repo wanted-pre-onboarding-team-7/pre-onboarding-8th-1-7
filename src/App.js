852103e97382f15c3e4b03c302686cd38d02eee1
이수창 <soo@isuchang-ui-MacBookPro.local>
2022-12-21 15:02:34 +0900

    Feat: 기본 설정 및 Router 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import {Route,Routes} from 'react-router-dom'
+import styled from "styled-components"
 function App() {
-  return <div className="App"></div>;
+
+  return (
+    <Main>
+     
+    <Routes>
+        <Route exact path="/"  element={<LoginPage/>} />
+    </Routes>     
+   
+   </Main>
+  );
 }
+
+const Main = styled.div`
+  display:flex;
+  min-height:100vh;
+  justify-content:center;
+  align-items:center;
+
+`
 
 export default App;
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+
+const LoginPage = () => {
+
+
+
+     
+    return (
+        <div>
+         
+        </div>
+    );
+};
+
+export default LoginPage;
+
