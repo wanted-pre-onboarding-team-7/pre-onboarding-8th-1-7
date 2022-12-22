e8b65bb4879ee1d2b9efc11691fb07e1d40055c2
sujin <ikosdu60@gmail.com>
2022-12-22 23:36:16 +0900

    Feat: TodoItem 컴포넌트 구현

---
+++ src/components/TodoItem.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+
+const TodoItem = ({ text, id, isCompleted }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [editedText, setEditedText] = useState('');
+  const [isCompleteByCheckBox, setIsCompleteByCheckBox] = useState(false);
+
+  const clickEdit = () => {
+    setIsEdit(!isEdit);
+  };
+
+  const clickEditDoneButton = async () => {
+    try {
+      if (isCompleteByCheckBox === true)
+        setIsCompleteByCheckBox(!isCompleteByCheckBox);
+      const propsData = {
+        id,
+        userData: { todo: editedText, isCompleted: isCompleted },
+      };
+      await putTodos(propsData); // TODO: F. Todo axios 함수 호출
+      setEditedText('');
+    } catch (err) {
+      console.error(err);
+      alert('요청하신 데이터를 처리할 수 없습니다. 관리자에게 문의해주세요.');
+    }
+  };
+
+  const clickRemoveButton = async () => {
+    try {
+      await deleteTodos(id); // TODO: F. Todo axios 함수 호출
+    } catch (err) {
+      console.error(err);
+      alert('요청하신 데이터를 처리할 수 없습니다. 관리자에게 문의해주세요.');
+    }
+  };
+  return isEdit ? (
+    <>
+      <input
+        autoFocus
+        type="text"
+        value={editedText}
+        onChange={(e) => {
+          setEditedText(e.target.value);
+        }}
+      />
+      <button type="button" onClick={() => clickEditDoneButton(false)}>
+        제출
+      </button>
+      <button type="button" onClick={clickEdit}>
+        취소
+      </button>
+    </>
+  ) : (
+    <>
+      <input
+        type="checkbox"
+        checked={isCompleteByCheckBox}
+        onChange={() => setIsCompleteByCheckBox(!isCompleteByCheckBox)}
+      />
+      <span>{text}</span>
+      <button type="button" onClick={clickEdit}>
+        수정
+      </button>
+      <button type="button" onClick={() => clickRemoveButton(id)}>
+        삭제
+      </button>
+    </>
+  );
+};
+
+export default TodoItem;
