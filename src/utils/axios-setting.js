a063f03f28b024a29bf6958eddde184304dec60a
jiyoonZ <wldbsl416@gmail.com>
2022-12-23 01:12:21 +0900

    Feat: 요청-응답 에러처리

--- src/utils/axios-setting.js
+++ src/utils/axios-setting.js
@@ -21,18 +21,66 @@ authAxios.interceptors.request.use((config) => {
 });
 
 authAxios.interceptors.response.use(
-  function (response) {
+  (response) => {
     return response;
   },
-  async function (error) {
+  async (error) => {
     const { response: errorResponse } = error;
+    const { request: errorResquest } = error;
     const errorResp = error.response.data.message;
-    // 인증 에러 발생시
-    if (errorResponse.status === 401 && errorResp === 'Unauthorized') {
-      alert('아이디와 비밀번호를 확인해주세요.');
-      window.location.href = '/';
+    if (errorResponse) {
+      // 인증 에러 발생시
+      if (errorResponse.status === 401 && errorResp === 'Unauthorized') {
+        alert('아이디와 비밀번호를 확인해주세요.');
+        window.location.href = '/';
+      } else {
+        alert('올바른 아이디와 비밀번호를 입력해주세요.');
+      }
+    } else if (errorResquest) {
+      alert(
+        '로그인/회원가입 요청이 거절되었습니다. 네트워크를 확인하거나 다시 시도해주세요',
+      );
     } else {
-      alert(errorResp);
+      alert(`${error.message} : 잘못된 요청입니다. 다시 시도해주세요.`);
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+export const todosAxios = axios.create({
+  baseURL,
+  headers,
+});
+
+todosAxios.interceptors.request.use((config) => {
+  const accessToken = getLocalStorageToken();
+  if (accessToken && config.headers) {
+    config.headers['Authorization'] = `Bearer ${accessToken}`;
+  }
+  return config;
+});
+
+todosAxios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const { response: errorResponse } = error;
+    const { request: errorResquest } = error;
+    const errorResp = error.response.data.message;
+    if (errorResponse) {
+      // 인증 에러 발생시
+      if (errorResponse.status === 401 && errorResp === 'Unauthorized') {
+        alert('로그아웃 되었습니다.다시 로그인 해주세요.');
+        window.location.href = '/';
+      } else {
+        alert('올바른 값을 입력해주세요.');
+      }
+    } else if (errorResquest) {
+      alert('잘못된 요청입니다. 네트워크를 확인하거나 다시 시도해주세요');
+    } else {
+      alert(`${error.message} : 잘못된 요청입니다. 다시 시도해주세요.`);
     }
 
     return Promise.reject(error);
