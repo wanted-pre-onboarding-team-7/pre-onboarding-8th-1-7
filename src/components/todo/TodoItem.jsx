dba72251366aa759790dfe4369f3ce93ae1f6d0a
sujin <soojin9877@naver.com>
2022-12-22 11:06:18 +0900

    Feat: 투두 수정 및 삭제 기능

--- src/components/todo/TodoItem.jsx
+++ src/components/todo/TodoItem.jsx
@@ -1,16 +1,82 @@
 import { useState } from 'react';
+import axios from 'axios';
+import { BASE_URL } from '../../constants/constants';
 
 function TodoItem({ text, id, isCompleted }) {
+  const token = localStorage.getItem('token');
+  const [update, setUpdate] = useState(false);
+  const [updateText, setUpdateText] = useState(text);
   const [complete, setComplete] = useState(isCompleted);
+
+  const handleDelete = async () => {
+    try {
+      const response = await axios.delete(`${BASE_URL}todos/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.status === 204) window.location.reload();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleUpdate = async (isCompleted, check) => {
+    try {
+      if (check === true) setComplete(!complete);
+      const response = await axios.put(
+        `${BASE_URL}todos/${id}`,
+        {
+          todo: updateText,
+          isCompleted: isCompleted,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      if (response.status === 200 && check === false) window.location.reload();
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <>
-      <input type="checkbox" checked={complete}></input>
-      <span className={complete ? 'completed' : ''}>{text}</span>
-      <button type="button">수정</button>
-      <button type="button">삭제</button>
+      {update ? (
+        <>
+          <input
+            type="text"
+            value={updateText}
+            onChange={(e) => {
+              setUpdateText(e.target.value);
+            }}
+          ></input>
+          <button type="button" onClick={() => handleUpdate(complete, false)}>
+            제출
+          </button>
+          <button type="button" onClick={() => setUpdate(false)}>
+            취소
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            type="checkbox"
+            checked={complete}
+            onChange={() => handleUpdate(!complete, true)}
+          ></input>
+          <span className={complete ? 'completed' : ''}>{text}</span>
+          <button type="button" onClick={() => setUpdate(true)}>
+            수정
+          </button>
+          <button type="button" onClick={handleDelete}>
+            삭제
+          </button>
+        </>
+      )}
     </>
   );
 }
-
 export default TodoItem;
