da4a0225d72c82d6742bcfda9dc736de0318eaee
jiyoonZ <wldbsl416@gmail.com>
2022-12-22 14:19:02 +0900

    Refactor: useRef로 input 관리하기

--- src/components/SignForm.js
+++ src/components/SignForm.js
@@ -4,19 +4,27 @@ import { theme } from '../theme';
 import { SIGN_IN, SUBMIT_BTN, SIGN_UP } from '../utils/constants';
 import TextInput from './TextInput';
 import { postSignin, postSignup } from '../utils/axios-api-fn';
+import { useNavigate } from 'react-router-dom';
 
 export default function SignForm({ signState, setSignState }) {
+  // useState 부분은 지환님 코드 합치면 삭제
   const [form, setForm] = useState(initialForm);
   const emailRef = useRef(initialForm.email);
   const passwordRef = useRef(initialForm.password);
-
+  const navigate = useNavigate();
   //수창님 부분 코드 : 로그인/회원가입 상태가 변경될때마다 input reset 시키려고..?
   useEffect(() => {
-    setForm(initialForm);
+    clearInput();
   }, [signState]);
+
+  const clearInput = () => {
+    emailRef.current.value = '';
+    passwordRef.current.value = '';
+  };
 
   // 지환님 부분 코드
   const handleChange = (e) => {
+    // useState 부분은 지환님 코드 합치면 삭제
     setForm((prev) => {
       const newForm = { ...prev, [e.target.id]: e.target.value };
       return newForm;
@@ -38,21 +46,27 @@ export default function SignForm({ signState, setSignState }) {
     };
     if (signState === SIGN_IN) {
       postSignin(formData);
+      navigate('/todo');
     }
     if (signState === SIGN_UP) {
       postSignup(formData);
+      setSignState(SIGN_IN);
     }
-
-    setSignState(SIGN_IN);
   };
 
   return (
     <Form onSubmit={handleSubmit}>
-      <TextInput type="email" value={form.email} onChange={handleChange} />
+      <TextInput
+        type="email"
+        value={emailRef.current.value}
+        onChange={handleChange}
+        refValue={emailRef}
+      />
       <TextInput
         type="password"
-        value={form.password}
+        value={passwordRef.current.value}
         onChange={handleChange}
+        refValue={passwordRef}
       />
       <SubmitBtn
         type="submit"
