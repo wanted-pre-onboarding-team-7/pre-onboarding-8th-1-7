18a48e91ac6327fcd83e7b13c0a4605fb105ecea
Quartz <skyu.dev@gmail.com>
2022-12-22 19:43:50 +0900

    Feat: /todo 경로의 페이지 컴포넌트 구현

--- src/pages/Todos.js
+++ src/pages/Todos.js
@@ -1,5 +1,47 @@
-function Todos() {
-  return <div>Todos</div>;
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { theme } from '../theme';
+
+export default function TodoScreen() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {}, []);
+
+  return (
+    <Wrapper bgColor={theme.bgColorlight}>
+      <Title>Todo list</Title>
+      <TodoWrapper>
+        {/* D. TodoForm 구현 */}
+        {todos.length === 0 ? (
+          <span>투두 리스트가 없습니다.</span>
+        ) : (
+          <span></span>
+        )}
+        {/* */}
+      </TodoWrapper>
+    </Wrapper>
+  );
 }
 
-export default Todos;
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: ${(props) => props.bgColor};
+  display: flex;
+  flex-direction: column;
+  /* justify-content: center; */
+  align-items: center;
+`;
+const TodoWrapper = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 32px;
+  font-weight: 600;
+  margin-bottom: 30px;
+  margin-top: 30px;
+`;
