0e47cc6fd75dd2b36e5ff5ba0636f24746e29ae4
sujin <soojin9877@naver.com>
2022-12-22 11:00:58 +0900

    Feat: 투두 입력 기능

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,9 +1,48 @@
+import { useState } from 'react';
 import { Navigate } from 'react-router-dom';
+import axios from 'axios';
+import { BASE_URL } from '../constants/constants';
 
 function Todo() {
   const token = localStorage.getItem('token');
+  const [text, setText] = useState('');
 
-  return <>{!token && <Navigate to="/" replace={true} />}</>;
+  const handleCreate = async () => {
+    try {
+      const response = await axios.post(
+        `${BASE_URL}todos`,
+        {
+          todo: text,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      if (response.status === 201) window.location.reload();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  return (
+    <>
+      {!token && <Navigate to="/" replace={true} />}
+      <main>
+        <h1>TODO LIST</h1>
+        <section>
+          <input
+            type="text"
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+          ></input>
+          <button type="button" onClick={handleCreate}>
+            추가
+          </button>
+        </section>
+      </main>
+    </>
+  );
 }
-
 export default Todo;
