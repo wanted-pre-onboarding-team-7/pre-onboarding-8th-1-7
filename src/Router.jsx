d878a6c722b849780e6262a54073f6d73b40272d
sujin <ikosdu60@gmail.com>
2022-12-22 09:10:42 +0900

    Feat: '/todo/ Route 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import SignIn from './ui/pages/SignIn';
 import SignUp from './ui/pages/SignUp';
+import TodoList from './ui/pages/TodoList';
 
 const Router = () => (
   <Routes>
     <Route path="/" element={<SignIn />} />
     <Route path="/signup" element={<SignUp />} />
+    <Route path="/todo" element={<TodoList />} />
   </Routes>
 );
 
