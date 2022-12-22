6ad32f9eb01c62d1eff1bc0f788b80a508ad5961
jiyoonZ <wldbsl416@gmail.com>
2022-12-22 12:59:12 +0900

    Feat: 투두 추가하기 기능 구현

--- src/components/todo/TodoForm.js
+++ src/components/todo/TodoForm.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 import { useState, useRef } from 'react';
+import { postTodo } from '../../api/api';
 
-function TodoForm() {
+function TodoForm({ refresh }) {
   const [newTodo, setNewTodo] = useState();
   const inputRef = useRef();
   const changeHadler = (evt) => {
@@ -10,11 +11,20 @@ function TodoForm() {
     } = evt;
     setNewTodo(value);
   };
-
+  const sumbitHandler = async (evt) => {
+    evt.preventDefault();
+    if (newTodo) {
+      await postTodo(newTodo);
+      inputRef.current.value = '';
+      inputRef.current.focus();
+      setNewTodo('');
+      refresh();
+    }
+  };
   return (
     <>
       {' '}
-      <Form>
+      <Form onSubmit={sumbitHandler}>
         <TodoInput
           ref={inputRef}
           placeholder="Add task"
@@ -31,7 +41,7 @@ const Form = styled.form`
   padding: 20px 0;
 `;
 const TodoInput = styled.input`
-  width: 72%;
+  width: 78%;
   height: 30px;
   border: none;
   background-color: #d1d1d1;
