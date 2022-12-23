4d57bc588236dbbcc2cbcc3a2d4bbda514880255
sujin <soojin9877@naver.com>
2022-12-23 18:53:45 +0900

    Chore: basename 제거

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Router from './routes/Router';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Router />
     </BrowserRouter>
   );
